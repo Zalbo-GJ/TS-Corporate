6e2bd7a62ad90aaee9d30602b89696571fc720f1
zalbo-gj <zjagema@gmail.com>
2024-11-11 18:35:15 +0300

    added links on the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ export default function NavBar() {
     <div className="bg-white">
       <nav className="container mx-auto p-4 flex  md:flex-row justify-between items-center">
         <div className="text-2xl text-black mb-4 md:mb-0">TS</div>
-        <ul className="flex items-center md:flex-row space-x-5 md:space-y-0 md:space-x-4">
+        <ul className="flex items-center md:flex-row space-x-3 md:space-y-0 md:space-x-8  text-xs md:text-lg ">
           <li>
             <a href="/#home" className="text-black hover:text-gray-400">
               Home
@@ -17,6 +17,19 @@ export default function NavBar() {
           <li>
             <a href="/#services" className="text-black hover:text-gray-400">
               Services
+            </a>
+          </li>{" "}
+          <li>
+            <a
+              href="/partners-and-clients"
+              className="text-black hover:text-gray-400"
+            >
+              Clients & Partners
+            </a>
+          </li>{" "}
+          <li>
+            <a href="/projects" className="text-black hover:text-gray-400">
+              Projects
             </a>
           </li>
           <li>
